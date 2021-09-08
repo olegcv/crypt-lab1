0b147dbd3db4f76c03db2f42458ee00d221407be
nichtDTF <66942395+nichtDTF@users.noreply.github.com>
2021-09-08 18:34:27 +0300

    Add files via upload

--- run.rb
+++ run.rb
@@ -3,3 +3,6 @@ require_relative 'text_worker'
 textworker = TextWorker.new('text.txt')
 hash_1 =  textworker.call.upcase.split('').tally
 hash_1.sort_by(&:last).each{|value| puts "#{value.first} : #{value.last}"}
+
+hash_2 = textworker.call.upcase.split(' ')
+a = ('А'..'Я').to_a.repeated_combination(2).to_a
