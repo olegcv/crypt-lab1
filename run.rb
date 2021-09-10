6446e5e07cd77f91cfa4e098ea4f11eba21735dc
Oleg C <66942395+olegcv@users.noreply.github.com>
2021-09-10 15:28:01 +0300

    Add files via upload

--- run.rb
+++ run.rb
@@ -3,7 +3,10 @@ require_relative 'text_worker'
 def get_single_letters
   textworker = TextWorker.new('text.txt')
   hash_1 =  textworker.call.upcase.split('').tally
-  hash_1.sort_by(&:last).each{|value| puts "#{value.first} : #{value.last} : #{value.last.to_f/hash_1.values.sum.to_f}"}
+  hash_1.sort_by(&:last).each do |value| 
+    res = value.last.to_f/hash_1.values.sum
+    puts "%5s    " % value.first + "    %5d    " % value.last + "    %5f    " % res + "    %5f   " % Math.log2(res).abs
+  end
   p "Result = #{hash_1.values.sum}"
 end
 
@@ -27,8 +30,8 @@ def get_pair_letters
   sum = result_hash.values.sum
   f = File.open('result.txt', 'w+')
   result_hash.each do |key, value|
-    result_hash[key] = value.to_f / sum
-    result = "%10s" % key + "%10d " % value + " %5f \n" % Math.log2(value).round(5)
+    result_hash[key] = value.to_f / sum.to_f
+    result = "%10s" % key + "    %10f    " % result_hash[key] + "    %5f \n" % Math.log2(value).round(5)
     f.write(result)
   end
 
